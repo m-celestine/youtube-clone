e919d814dc1f42c5d96bcf1d95540ca4cadbe0e6
m-celestine <mjk.celestine@gmail.com>
2026-04-27 17:42:05 -0400

    remove space

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -27,28 +27,27 @@ export const createUser = functions.auth.user().onCreate((user: UserRecord) => {
 
 
 export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
-    
-    // Check if the user is authentication
-    if (!request.auth) {
-        throw new functions.https.HttpsError(
-        "failed-precondition",
-        "The function must be called while authenticated."
-        );
-    }
-    
-    const auth = request.auth;
-    const data = request.data;
-    const bucket = storage.bucket(rawVideoBucketName);
+  // Check if the user is authentication
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
 
-    // Generate a unique filename for upload
-    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
 
-    // Get a v4 signed URL for uploading file
-    const [url] = await bucket.file(fileName).getSignedUrl({
-        version: "v4",
-        action: "write",
-        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-    });
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
-    return {url, fileName};
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
